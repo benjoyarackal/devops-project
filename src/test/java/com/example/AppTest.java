f44b949394162e58c10b689a9415cfcfc9bb999d
Ben Joy <benjoyarakal@gmail.com>
2025-04-13 00:20:44 -0400

    Added WelcomeController for Issue #1

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertTrue("App should return true", true);
+    }
+}
